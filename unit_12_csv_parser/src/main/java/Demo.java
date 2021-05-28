f06980b0d53ed2db2442bb168aeddb8c2331a0ea
nhrin <grinkonu@gmail.com>
2021-05-29 01:22:11 +0300

    modify Demo unit 12 parse CSV

--- unit_12_csv_parser/src/main/java/Demo.java
+++ unit_12_csv_parser/src/main/java/Demo.java
@@ -11,9 +11,17 @@ public class Demo {
 
         System.out.println("Getting element by number row and number column:");
         System.out.println(parser.getField(1,3));
+        System.out.println();
 
         System.out.println("Getting element by number row and name column:");
         System.out.println(parser.getField(1,"model"));
+        System.out.println();
+
+        System.out.println("Getting titles");
+        for (String s : parser.getTitles()) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
 
         List<DataTest> list = setter.getObjectsFromCSV(parser, DataTest.class);
         for (DataTest dataTest : list) {
